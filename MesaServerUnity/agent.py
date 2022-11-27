1319c5631abf6ae897e4f4cdd33243fd2e0c45dc
A01745907 <a01745907@tec.mx>
2022-11-26 22:10:23 -0600

    añade cambios de agente y modelo

--- MesaServerUnity/agent.py
+++ MesaServerUnity/agent.py
@@ -1,11 +1,14 @@
 from mesa import Agent
+from math import sqrt, pow
+
 
 class Car(Agent):
     """
     Agent that moves randomly.
     Attributes:
-        unique_id: Agent's ID 
-        direction: Randomly chosen direction chosen from one of eight directions
+        unique_id: Agent's ID
+        direction: Randomly chosen direction
+        chosen from one of eight directions
     """
     def __init__(self, unique_id, model):
         """
@@ -15,24 +18,302 @@ class Car(Agent):
             model: Model reference for the agent
         """
         super().__init__(unique_id, model)
+        self.prevSentido = ""
+        self.tipo = "car"
+        self.nexcord = ()
+        self.destino = None
+        self.prevcord = ()
+        self.parado = False
+        self.mepuedomover = True
+        self.entrada = None
+        self.NoDestino = True
+        self.NoEntrada = True
 
     def move(self):
-        """ 
+        """
         Determines if the agent can move in the direction that was chosen
-        """        
-        self.model.grid.move_to_empty(self)
+        """
+               
+        possibleSteps = self.model.grid.get_neighborhood(
+            self.pos,
+            moore=False,
+            include_center=False,
+            radius=1)
+
+        cord = list(self.pos)
+        cordstr = str(cord)
+        entradastr = str(list(self.entrada))
+        print(type(self.entrada))
+        for e in possibleSteps:
+            actuale = list(e)
+            if actuale == self.entrada and self.NoEntrada:
+                print("ARRIBA spuanchi")
+                print(f'Encontraste entrada {actuale} == {self.entrada}')
+                self.nexcord = e
+                self.NoEntrada = False
+            destinoE = list(e)
+            if destinoE == self.destino:
+                print(f'Encontraste destino {destinoE} == {self.destino}')
+                self.nexcord = e
+                self.NoDestino = False
+
+        if self.NoDestino and self.NoEntrada:
+            for i in possibleSteps:
+                cellmates = self.model.grid.get_cell_list_contents(i)
+                for j in cellmates:
+                    if j.tipo == "semaforo" and j.state is False:
+                        # self.parado = True
+                        if self.prevSentido == ">":
+                            if self.pos == j.pos or self.pos[0] < j.pos[0]\
+                                and self.pos[1] == j.pos[1]:
+                                self.nexcord = self.pos
+                        elif self.prevSentido == "<":
+                            if self.pos == j.pos or self.pos[0] > j.pos[0]\
+                                and self.pos[1] == j.pos[1]:
+                                self.nexcord = self.pos
+                        elif self.prevSentido == "^":
+                            if self.pos == j.pos or self.pos[1] < j.pos[1]\
+                                and self.pos[0] == j.pos[0]:
+                                self.nexcord = self.pos
+                        elif self.prevSentido == "v":
+                            if self.pos == j.pos or self.pos[1] > j.pos[1]\
+                                and self.pos[0] == j.pos[0]:
+                                self.nexcord = self.pos
+                    elif j.tipo == "semaforo" and j.state is True:
+                        self.parado = False
+                        if self.prevSentido == "<":
+                            self.nexcord = ((cord[0] - 1), cord[1])
+                        elif self.prevSentido == ">":
+                            self.nexcord = ((cord[0] + 1), cord[1])
+                        elif self.prevSentido == "v":
+                            self.nexcord = (cord[0], (cord[1] - 1))
+                        elif self.prevSentido == "^":
+                            self.nexcord = (cord[0], (cord[1] + 1))
+                    elif j.tipo == "calle":
+                        if cordstr in self.model.dicSentido:
+                            sentido = self.model.dicSentido[cordstr]
+                            self.prevSentido = sentido
+                            if sentido == "<":
+                                self.nexcord = ((cord[0] - 1), cord[1])
+                            elif sentido == ">":
+                                self.nexcord = ((cord[0] + 1), cord[1])
+                            elif sentido == "v":
+                                self.nexcord = (cord[0], (cord[1] - 1))
+                            elif sentido == "^":
+                                self.nexcord = (cord[0], (cord[1] + 1))
+                            elif sentido == "c":
+                                distanciaActual = 10000000000000000
+                                for k in possibleSteps:
+                                    # Evaluo que no sea la posición pasada
+                                    if k != self.prevcord:
+                                        cellmates = self.model.grid.\
+                                            get_cell_list_contents(k)
+                                        for n in cellmates:
+                                            if n.tipo == "calle" or\
+                                                n.tipo == "semaforo":
+                                                disObjetivo = self.\
+                                                    euclidiana(self.destino, k)
+                                                if distanciaActual > disObjetivo:
+                                                    sentido2 = self.model.\
+                                                        dicSentido[str(list
+                                                                (n.pos))]
+                                                    val = self.\
+                                                        validarmov(sentido2,
+                                                                list(n.pos),
+                                                                cord)
+                                                    if val:
+                                                        distanciaActual =\
+                                                            disObjetivo
+                                                        Ncord = list(n.pos)
+                                self.nexcord = (Ncord[0], Ncord[1])
+            self.prevcord = self.pos
+
+    def cambiodecarril(self,
+                       CordO: list,
+                       PS: list,
+                       SCasilla: str,
+                       Destino: list) -> list:
+        result: list = []
+        XOrigen = CordO[0]
+        YOrigen = CordO[1]
+        for i in PS:
+            cellmates = self.model.grid.get_cell_list_contents(i)
+            for j in cellmates:
+                if SCasilla == "<" and XOrigen == i[0] and YOrigen != i[1] and\
+                   j.tipo == "calle":
+                    disObjetivo1 = self.euclidiana(Destino, i)
+                    disObjetivo2 = self.euclidiana(Destino, CordO)
+                    if disObjetivo1 > disObjetivo2:
+                        return [False, 0]
+                    else:
+                        arribaAbajo = YOrigen - i[1]
+                        if arribaAbajo < 0:
+                            # Mover arriba
+                            return [True, 1]
+                        else:
+                            # Mover abajo
+                            return [True, -1]
+
+                elif SCasilla == ">" and XOrigen == i[0] and\
+                        YOrigen != i[1] and\
+                        j.tipo == "calle":
+                    disObjetivo1 = self.euclidiana(Destino, i)
+                    disObjetivo2 = self.euclidiana(Destino, CordO)
+                    if disObjetivo1 > disObjetivo2:
+                        return [False, 0]
+                    else:
+                        arribaAbajo = YOrigen - i[1]
+                        if arribaAbajo < 0:
+                            # Mover arriba
+                            return [True, 1]
+                        else:
+                            # Mover abajo
+                            return [True, -1]
+
+                elif SCasilla == "v" and YOrigen == i[1] and\
+                        XOrigen != i[0] and\
+                        j.tipo == "calle":
+                    disObjetivo1 = self.euclidiana(Destino, i)
+                    disObjetivo2 = self.euclidiana(Destino, CordO)
+                    if disObjetivo1 > disObjetivo2:
+                        return [False, 0]
+                    else:
+                        izDer = XOrigen - i[0]
+                        if izDer < 0:
+                            # Mover derecha
+                            return [True, 1]
+                        else:
+                            # Mover izquierda
+                            return [True, -1]
+
+                elif SCasilla == "^" and YOrigen == i[1] and\
+                        XOrigen != i[0] and\
+                        j.tipo == "calle":
+                    disObjetivo1 = self.euclidiana(Destino, i)
+                    disObjetivo2 = self.euclidiana(Destino, CordO)
+                    if disObjetivo1 > disObjetivo2:
+                        return [False, 0]
+                    else:
+                        izDer = XOrigen - i[0]
+                        if izDer < 0:
+                            # Mover derecha
+                            return [True, 1]
+                        else:
+                            # Mover izquierda
+                            return [True, -1]
+        return result
+
+    def euclidiana(self, Edestino: list, Ek: list) -> float:
+        return sqrt(pow(Edestino[0] - Ek[0], 2) +
+                    pow((Edestino[1] - Ek[1]), 2))
+
+    def validarmov(self, SCasilla: str, Objetivo: list, Origen: list) -> bool:
+        XOrigen = Origen[0]
+        YOrigen = Origen[1]
+        XObjetivo = Objetivo[0]
+        YObjetivo = Objetivo[1]
+        if SCasilla == "<":
+            if XObjetivo < XOrigen:
+                return True
+            else:
+                return False
+        elif SCasilla == ">":
+            if XObjetivo > XOrigen:
+                return True
+            else:
+                return False
+        elif SCasilla == "v":
+            if YOrigen > YObjetivo:
+                return True
+            else:
+                False
+        elif SCasilla == "^":
+            if YOrigen < YObjetivo:
+                return True
+            else:
+                False
+        elif SCasilla == "c":
+            return True
+        return False
 
     def step(self):
-        """ 
+        """
         Determines the new direction it will take, and then moves
         """
         self.move()
+        '''
+        # contador = 0
+        # agente = [agent for agent in self.model.schedule.agents
+        #           if agent.tipo == "car" and
+        #           agent.unique_id != self.unique_id and
+        #           agent.nexcord == self.nexcord]
+        # print(len(agente))
+        # if len(agente) != 0:
+        #     contador = contador + 1
+        # print(f'contador {contador}')
+
+        #     if agente[0].parado == True:
+        #         self.parado = True
+        #     else:
+        #         if self.unique_id > agente[0].unique_id:
+        #             self.parado = False
+        #         else:
+        #             self.parado = True
+        # for x in agente:
+        #     if x.nexcord == self.nexcord:
+        #         if x.parado:
+        #             # Esta x en un semaforo en rojo
+        #             print("Quieto, el de enfrente esta parado")
+        #             print(f'Prev cod de self {self.prevcord}')
+        #             print(f'Coorde de x {x.pos} y next {x.nexcord}')
+        #             print(self.nexcord)
+        #             print(self.prevcord)
+        #             self.parado = True
+        #         elif not(x.parado):
+        #             print(f'Id de x= {x.unique_id} , pos {x.pos}')
+        #             print(f'id de Self= {self.unique_id}, pos {x.pos}')
+        #             if self.unique_id > x.unique_id:
+        #                 self.parado = False
+        #             else:
+        #                 self.parado = True
+        '''
+ 
+    def advance(self) -> None:
+        contador = 0
+        self.mepuedomover = True
+
+        agente = [agent for agent in self.model.schedule.agents
+                  if agent.tipo == "car" and
+                  agent.unique_id != self.unique_id and
+                  agent.nexcord == self.nexcord]
+        agentespos = [agent for agent in self.model.schedule.agents
+                      if agent.tipo == "car" and
+                      agent.unique_id != self.unique_id and
+                      agent.pos == self.nexcord]
+        print(len(agente))
+        if len(agente) != 0:
+            contador = contador + 1
+        print(f'contador {contador}')
+        if contador == 0:
+            self.model.grid.move_agent(self, self.nexcord)
+        else:
+            ...
+            if self.parado:
+                self.mepuedomover = False
+                ...
+            else:
+                if self.unique_id > agente[0].unique_id:
+                    self.parado = False
+                    self.model.grid.move_agent(self, self.nexcord)
+                else:
+                    self.parado = True
+
 
 class Traffic_Light(Agent):
     """
     Traffic light. Where the traffic lights are in the grid.
     """
-    def __init__(self, unique_id, model, state = False, timeToChange = 10):
+    def __init__(self, unique_id, model, state=False, timeToChange=10):
         super().__init__(unique_id, model)
         """
         Creates a new Traffic light.
@@ -40,18 +321,26 @@ class Traffic_Light(Agent):
             unique_id: The agent's ID
             model: Model reference for the agent
             state: Whether the traffic light is green or red
-            timeToChange: After how many step should the traffic light change color 
+            timeToChange: After how many step
+                          should the traffic light change color
         """
         self.state = state
         self.timeToChange = timeToChange
+        self.tipo = "semaforo"
+
+    def modificarcolor(self):
+        self.compañero = []
+        self.vecinos = []
 
     def step(self):
-        """ 
-        To change the state (green or red) of the traffic light in case you consider the time to change of each traffic light.
+        """
+        To change the state (green or red) of the traffic light in case
+        you consider the time to change of each traffic light.
         """
         # if self.model.schedule.steps % self.timeToChange == 0:
         #     self.state = not self.state
         pass
+
 
 class Destination(Agent):
     """
@@ -59,9 +348,11 @@ class Destination(Agent):
     """
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
+        self.tipo = "destino"
 
     def step(self):
         pass
+
 
 class Obstacle(Agent):
     """
@@ -69,15 +360,17 @@ class Obstacle(Agent):
     """
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
+        self.tipo = "edificio"
 
     def step(self):
         pass
+
 
 class Road(Agent):
     """
     Road agent. Determines where the cars can move, and in which direction.
     """
-    def __init__(self, unique_id, model, direction= "Left"):
+    def __init__(self, unique_id, model, direction="Left"):
         """
         Creates a new road.
         Args:
@@ -87,6 +380,8 @@ class Road(Agent):
         """
         super().__init__(unique_id, model)
         self.direction = direction
+        self.tipo = "calle"
+        self.parada = False
 
     def step(self):
         pass
--- MesaServerUnity/model.py
+++ MesaServerUnity/model.py
@@ -1,11 +1,12 @@
 from mesa import Model
-from mesa.time import RandomActivation
+from mesa.time import SimultaneousActivation
 from mesa.space import MultiGrid
 from agent import *
 import json
 
+
 class RandomModel(Model):
-    """ 
+    """
     Creates a new model with random agents.
     Args:
         N: Number of agents in the simulation
@@ -13,37 +14,83 @@ class RandomModel(Model):
     def __init__(self, N):
 
         dataDictionary = json.load(open("mapDictionary.json"))
-
+        initCar = []
+        numcar = 0
+        self.destino = []
         self.traffic_lights = []
+        self.dicSentido = {}
+        self.dicEntrada = {'[3, 22]': [3, 23],
+                           '[21, 22]': [21, 23],
+                           '[12, 20]': [13, 20],
+                           '[18, 20]': [17, 20],
+                           '[3, 19]': [3, 18],
+                           '[2, 15]': [1, 15],
+                           '[5, 15]': [6, 15],
+                           '[12, 15]': [13, 15],
+                           '[18, 14]': [17, 14],
+                           '[10, 7]': [10, 8],
+                           '[21, 5]': [22, 5],
+                           '[5, 4]': [6, 4],
+                           '[12, 4]': [13, 4],
+                           '[19, 2]': [19, 1]}
 
         with open('2022_base.txt') as baseFile:
             lines = baseFile.readlines()
             self.width = len(lines[0])-1
             self.height = len(lines)
 
-            self.grid = MultiGrid(self.width, self.height, torus = False) 
-            self.schedule = RandomActivation(self)
+            self.grid = MultiGrid(self.width, self.height, torus=False)
+            self.schedule = SimultaneousActivation(self)
 
+            # Este for lee el archivo txt para dibujar el mapa
             for r, row in enumerate(lines):
                 for c, col in enumerate(row):
-                    if col in ["v", "^", ">", "<"]:
-                        agent = Road(f"r_{r*self.width+c}", self, dataDictionary[col])
+                    # Si el vaor es una calle es porque tiene estas flechas
+                    if col in ["v", "^", ">", "<", "c"]:
+                        # DataDictionary tiene el sentido de la calla
+                        agent = Road(f"r_{r*self.width+c}", self,
+                                     dataDictionary[col])
                         self.grid.place_agent(agent, (c, self.height - r - 1))
+                        initCar.append([c, self.height - r - 1])
+                        key = str([c, self.height - r - 1])
+                        self.dicSentido[key] = col
 
+                    # Genera los agentes SEMAFORO
                     elif col in ["S", "s"]:
-                        agent = Traffic_Light(f"tl_{r*self.width+c}", self, False if col == "S" else True, int(dataDictionary[col]))
+                        agent = Traffic_Light(f"tl_{r*self.width+c}",
+                                              self,
+                                              False if col == "S"
+                                              else True,
+                                              int(dataDictionary[col]))
                         self.grid.place_agent(agent, (c, self.height - r - 1))
                         self.schedule.add(agent)
                         self.traffic_lights.append(agent)
 
+                    # Genera los edificios
                     elif col == "#":
                         agent = Obstacle(f"ob_{r*self.width+c}", self)
                         self.grid.place_agent(agent, (c, self.height - r - 1))
 
+                    # Genera los puentos de destino
                     elif col == "D":
                         agent = Destination(f"d_{r*self.width+c}", self)
                         self.grid.place_agent(agent, (c, self.height - r - 1))
+                        self.destino.append([c, self.height - r - 1])
+        # Generar Carros
+        for i in range(5):
+            numcar += 1
+            ran = self.random.choice(initCar)
+            car = Car(numcar, self)
+            self.grid.place_agent(car, (ran[0], ran[1]))
+            self.schedule.add(car)
+            car.destino = self.random.choice(self.destino)
+            car.entrada = self.dicEntrada[str(car.destino)]
+            print(f'Destinos {car.destino} del carro ubicado en {car.pos}')
+            print(f'Entrada {car.entrada} del carro ubicado en {car.pos}')
 
+        # for i in self.dicSentido:w
+        #     print(i)
+        #     print(self.dicSentido[i])
         self.num_agents = N
         self.running = True
 
